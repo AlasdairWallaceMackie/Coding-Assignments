3b044d69160de69bfc05ba390c39a663df30a99f
AlasdairWallaceMackie <73504675+AlasdairWallaceMackie@users.noreply.github.com>
2021-02-14 10:55:46 -0500

    Update selection_sort.py

--- selection_sort.py
+++ selection_sort.py
@@ -1,16 +1,12 @@
 def selection_sort(ul):
-    min_value = ul[0]
-    
-    for i in range( len(ul) ):
-        flip_index = i
-        for j in range(i, len(ul) ):
-            if ul[j] < ul[i]:
-                min_value = ul[j]
-                flip_index = j
-        if flip_index > i:
-            ul[i], ul[flip_index] = ul[flip_index], ul[i]
+    for i in range( len(ul)-1 ):
+        min_value_index = i
+        for j in range(i+1, len(ul)):
+            if( ul[j] < ul[min_value_index] ):
+                min_value_index = j
+        ul[i], ul[min_value_index] = ul[min_value_index], ul[i]
         print(ul)
+    return ul
 
-selection_sort([8,7,6,5,4,3,2,1])
-
-
+# selection_sort([8,7,6,5,4,3,2,1])
+print( selection_sort([3,5,7,3,4,8,9,3,2,1]) )
